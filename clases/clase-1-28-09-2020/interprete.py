49e254d6e058304260d566c0da18832ec6b84295
Facundo Prieto <facundo.prieto321@gmail.com>
2020-09-28 20:39:34 -0300

    clase practica

---
+++ clases/clase-1-28-09-2020/interprete.py
@@ -0,0 +1,18 @@
+# Variales
+X = 1
+print(X)
+print(X + 1)
+
+Z = 2
+Y = 3
+
+print(Y + Z)
+
+print(X)
+X = Z + Y
+print(X)
+
+# Concatenation
+print('Hola ' + 'Mundo')
+print('Hola ' * 2)
+# print('Hola ' / 2) # la division da error
