3ecc01da7de0f1c1d306de3e3686b67ad84cbe91
Facundo Prieto <facundo.prieto321@gmail.com>
2020-09-29 17:16:34 -0300

    Ej 1.3

--- guia-de-ejercicios/ejercicios-1.py
+++ guia-de-ejercicios/ejercicios-1.py
@@ -11,3 +11,88 @@ def multipicacion(a, b):
 
 print("multipicacion(3, 2)")
 print(multipicacion(3, 2))
+
+# ej 1.3
+print('')
+print('ej 1.3')
+print('')
+
+print('a-')
+
+
+def perimetro(base, altura):
+    return base * 2 + altura * 2
+
+
+print("perimetro(2, 3)")
+print(perimetro(2, 3))
+
+print('')
+print('b-')
+
+
+def area(base, altura):
+    return multipicacion(base, altura)
+
+
+print("area(2, 3)")
+print(area(2, 3))
+
+print('')
+print('c-')
+
+
+def areaCordenadas(x1, x2, y1, y2):
+    base = x2 - x1
+    altura = y2 - y1
+    return base * altura
+
+
+print("areaCordenadas(1,4,2,4)")
+print(areaCordenadas(1, 4, 2, 4))
+
+print('')
+print('d-')
+
+
+def circuloPerimetro(radio):
+    return 2 * math.pi * radio
+
+
+print("circuloPerimetro(3)")
+print(circuloPerimetro(3))
+
+print('')
+print('e-')
+
+
+def circuloArea(radio):
+    return math.pi * radio ** 2
+
+
+print("circuloArea(3)")
+print(circuloArea(3))
+
+print('')
+print('f-')
+
+
+def esferaVolumen(radio):
+    return 4/3 * math.pi * radio ** 3
+
+
+print('esferaVolumen(3)')
+print(esferaVolumen(3))
+
+print('')
+print('g-')
+
+
+def trianguloHipotenusa(catetoOpuesto, catetoAdyacente):
+    return math.sqrt(catetoOpuesto ** 2 + catetoAdyacente ** 2)
+
+
+print('trianguloHipotenusa(5, 5)')
+print(trianguloHipotenusa(5, 5))
+
+
