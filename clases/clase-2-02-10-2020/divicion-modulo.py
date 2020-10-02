06cd84a0a8782ae3d8976eea0cfa8a48ec6ae21b
Facundo Prieto <facundo.prieto321@gmail.com>
2020-10-02 20:47:56 -0300

    teoria clase practica

---
+++ clases/clase-2-02-10-2020/divicion-modulo.py
@@ -0,0 +1,9 @@
+print(5 / 2)  # resultado completo
+print(5 // 2)  # resultado entero
+print(5 % 2)  # resto del resultado entero
+
+# ejemplo de uso
+
+# Ver si es par o no
+print(15 % 2 == 0)
+print(14 % 2 == 0)
