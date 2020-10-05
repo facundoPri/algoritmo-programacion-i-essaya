4aea320b82fdeef7ef198edec5fe5deb4331ecfc
Facundo Prieto <facundo.prieto321@gmail.com>
2020-10-05 20:57:57 -0300

    Correcciones de ejercicio

--- guia_de_ejercicios/ej_2/ej_2_10.py
+++ guia_de_ejercicios/ej_2/ej_2_10.py
@@ -1,7 +1,7 @@
 def imprimir_fichas_domino(numero):
     for x in range(0, numero+1):
-        for i in range(0, numero+1):
+        for i in range(x, numero+1):
             print(x, "|", i)
 
 
-imprimir_fichas_domino(10)
+imprimir_fichas_domino(3)
